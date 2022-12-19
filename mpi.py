e43424d9c37eb8772be6ffa148648199e21262a4
Percon12 <fcorreiaandrade@gmail.com>
2022-12-19 20:08:45 -0300

    MPI Funcionando

--- mpi.py
+++ mpi.py
@@ -9,25 +9,24 @@ cont1 = 0
 cont2 = 0
 
 if my_rank != 0:
-    with open(r'arq.txt','r') as arq:
+    #print('entrei no rank 0')
+    arq =  open('arq.txt')
+    arqread = arq.read()
+    lines = arqread.split()
+    number_of_words += len(lines)
+    for j in lines:
+        #print("entrei no for")
+        if j == " " or "\n" or "\t":
+            if len(j) < 6:
+                cont1=cont1+1
+            elif len(j) >= 6 and len(j) <= 10:
+                cont2=cont2+1
+    arq.close()
 
-        data = arq.read()
-        lines = arq.split()
-        number_of_words += len(lines)
-
-        for j in lines:
-            if j == " " or "\n" or "\t":
-                if len(j) < 6:
-                    cont1=cont1+1
-                elif len(j) >= 6 and len(j) <= 10:
-                    cont2=cont2+1
-        
-
-        message = "\nMenos que 6: ",cont1 + "\nEntre 6 e 10: ",cont2 + "\nTotal de palavras: ",number_of_words + "\nMeu rank: "+str(my_rank)
-        comm.send(message, dest=0)
-        arq.close()
+    message = "\nMenos que 6: "+str(cont1) + "\nEntre 6 e 10: "+str(cont2) + "\nTotal de palavras: "+str(number_of_words) + "\nMeu rank: "+str(my_rank)
+    comm.send(message, dest=0)
 else:
     for procid in range (1,p):
         message = comm.recv(source=procid)
-        print ("process 0 receives message from process", \
-            procid, ":", message)
+        print ("Proc ID:",procid, message)
+        print("\n")
